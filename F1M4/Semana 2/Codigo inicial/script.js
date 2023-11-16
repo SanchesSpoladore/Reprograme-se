78535a9d3df557386a37aacd05752a9ed558e151
Sanches Spoladore <sanches.lvs@gmail.com>
2023-11-16 19:47:57 -0300

    Correção do código inicial

--- F1M4/Semana 2/Codigo inicial/script.js
+++ F1M4/Semana 2/Codigo inicial/script.js
@@ -3,31 +3,16 @@ var cachorros = [{ "nome": "Thor", "imagem": "dog1" }, { "nome": "Zeca", "imagem
 var gatos = [{ "nome": "Dunga", "imagem": "cat1" }, { "nome": "Mika", "imagem": "cat2" }, { "nome": "Conan", "imagem": "cat3" }, { "nome": "Messi", "imagem": "cat4" }];
 
 // Selecionar elemento 'select_animais' ( 1 ponto)
-var animais = document.getElementById("select_animais")
 // Selecionar elemento 'container' ( 1 ponto)
-document.getElementById("container")
 // Selecionar elemento 'nome' ( 1 ponto)
-document.getElementById("nome")
 
 // Adicionar Listener 'input' ao elemento 'select_animais' (1 ponto)
-animais.addEventListener("input", listarAnimais,false);
-
 // Adicionar Listener 'mouseover' ao elemento 'container' (1 ponto)
 // Adicionar Listener 'mouseout' ao elemento 'container' (1 ponto)
 
 function listarAnimais() { // Implementação da função (10 pontos)
   limparDivContainer();
   // Implementar...
-    if (animais.value == "cat"){
-        for (i = 0; i <= 4; i++){
-            var imagem = document.createElement("img")
-            
-            
-        }
-    }
-    else {
-        alert("dog")
-    }
 }
 
 function mostrarNome(e) { // Implementação da função (4 pontos)
